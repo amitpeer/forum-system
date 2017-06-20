269f5994813734b991fb4201359223770d913220
stavlut <stavlut@gmail.com>
2017-06-20 16:51:14 +0300

    fh

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,11 @@
 package sample;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Controller {
 
+    public void pressButton(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
 }
