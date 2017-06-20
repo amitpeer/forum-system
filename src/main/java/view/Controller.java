f5311f3a08eb23c7d519550d798e50d3b8fff2cd
stavlut <stavlut@gmail.com>
2017-06-20 17:46:35 +0300

    Merge branch 'master' of https://github.com/amitpeer/forum-system
    # Conflicts:
    #	src/sample/Main.java

--- src/main/java/view/Controller.java
+++ src/main/java/view/Controller.java
@@ -1,7 +1,11 @@
 package view;
-
+import javafx.event.ActionEvent;
 /**
  * Created by Stav on 20/06/2017.
  */
 public class Controller {
+
+    public void pressButton(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,7 +1,24 @@
 package view;
 
-/**
- * Created by Stav on 20/06/2017.
- */
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
