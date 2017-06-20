a747d923451c40e36684bbecc31cd9474724576f
amitp <amitpeer05@gmail.com>
2017-06-20 16:42:04 +0300

    States

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,16 +1,23 @@
 package sample;
 
+import controller.IController;
+import controller.states.ActiveState;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.IModel;
+import model.Model;
+import view.IView;
 
-public class Main extends Application {
+public class Main extends Application implements IView {
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    private IController controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -18,6 +25,12 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        startMVC();
+    }
 
+    private void startMVC() {
+        IModel model = new Model();
+        IController controller = new ActiveState(this, model);
+        model.setController(controller);
     }
 }
